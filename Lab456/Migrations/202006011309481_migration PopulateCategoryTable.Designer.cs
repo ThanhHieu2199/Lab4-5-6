cf773a52f3382b5e432c226854ae40e2c73ded1d
hieu pham <phamthanhhieu20101999@gmail.com>
2020-06-01 22:01:07 +0700

    ViewModel

--- Lab456/Migrations/202006011309481_migration PopulateCategoryTable.Designer.cs
+++ Lab456/Migrations/202006011309481_migration PopulateCategoryTable.Designer.cs
@@ -7,13 +7,13 @@ namespace Lab456.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class CreateTableCourse : IMigrationMetadata
+    public sealed partial class migrationPopulateCategoryTable : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(CreateTableCourse));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(migrationPopulateCategoryTable));
         
         string IMigrationMetadata.Id
         {
-            get { return "202006011108029_CreateTableCourse"; }
+            get { return "202006011309481_migration PopulateCategoryTable"; }
         }
         
         string IMigrationMetadata.Source
